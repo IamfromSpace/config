7d172d940458cf176f93c5b7afa537564b86eded
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2018-03-10 22:10:25 -0800

    Add xmobar config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,12 +4,15 @@ import Control.Monad (ap)
 import qualified Data.List as List
 import Data.Map (fromList)
 import Data.Monoid (mappend)
+import System.IO
 import XMonad
 import XMonad.Actions.CycleWS
        (nextWS, prevWS, shiftToNext, shiftToPrev)
 import XMonad.Actions.Navigation2D
 import XMonad.Actions.SpawnOn
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.Script
 import XMonad.Layout
 import XMonad.Layout.BinarySpacePartition
@@ -27,8 +30,10 @@ import XMonad.Layout.WindowNavigation
 import qualified XMonad.StackSet as W
 import qualified XMonad.StackSet as Set
 import XMonad.Util.EZConfig
+import XMonad.Util.Run (spawnPipe)
 
 myLayout =
+    avoidStruts $
     windowNavigation $
     smartBorders $
     onWorkspace "1:browsing" (OneBig (15 / 16) (15 / 16)) $
@@ -37,22 +42,30 @@ myLayout =
   where
     tiled = Tall 1 (5 / 100) (7 / 16)
 
-main =
+main = do
+    xmproc <- spawnPipe "xmobar"
     xmonad $
-    withNavigation2DConfig def $
-    def
-    { workspaces = ["1:browsing", "2", "3", "4", "5", "6", "7", "8", "9"]
-    , logHook = fadeInactiveLogHook 0.8
-    , borderWidth = 2
-    , normalBorderColor = "#000000"
-    , focusedBorderColor = "#00FF00"
-    , focusFollowsMouse = False
-    , modMask = mod4Mask
-    , layoutHook = myLayout
-    } `removeKeys`
-    removedKeys `additionalKeysP`
-    specialKeys `additionalKeys`
-    normalKeys
+        withNavigation2DConfig def $
+        def
+        { workspaces = ["1:browsing", "2", "3", "4", "5", "6", "7", "8", "9"]
+        , logHook =
+              fadeInactiveLogHook 0.8 <+>
+              dynamicLogWithPP
+                  xmobarPP
+                  { ppOutput = hPutStrLn xmproc
+                  , ppTitle = xmobarColor "green" "" . shorten 50
+                  }
+        , borderWidth = 2
+        , normalBorderColor = "#000000"
+        , focusedBorderColor = "#00FF00"
+        , focusFollowsMouse = False
+        , modMask = mod4Mask
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = myLayout
+        } `removeKeys`
+        removedKeys `additionalKeysP`
+        specialKeys `additionalKeys`
+        normalKeys
   where
     removedKeys =
         [ (mod4Mask .|. shiftMask, xK_m)
